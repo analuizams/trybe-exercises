afdbc2965ad9caa59291971db329aeaa96ecf469
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2020-11-26 18:06:07 -0300

    Adiciona exercicio 9

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio9.js
@@ -0,0 +1,9 @@
+let numbers = [];
+
+for (index = 1; index <= 25; index += 1) {
+    numbers.push(index);
+}
+
+for (index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index] / 2)
+}
