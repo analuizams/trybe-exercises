ab67c75b3347d5f4ae5dacd33419c7f51ec99c8f
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2020-11-26 18:29:28 -0300

    Adiciona bonus e corrige 'let index'

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio8.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio8.js
@@ -1,6 +1,6 @@
 let numbers = [];
 
-for (index = 1; index <= 25; index += 1) {
+for (let index = 1; index <= 25; index += 1) {
     numbers.push(index);
 }
 
--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio9.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio9.js
@@ -1,9 +1,9 @@
 let numbers = [];
 
-for (index = 1; index <= 25; index += 1) {
+for (let index = 1; index <= 25; index += 1) {
     numbers.push(index);
 }
 
-for (index = 0; index < numbers.length; index += 1) {
+for (let index = 0; index < numbers.length; index += 1) {
     console.log(numbers[index] / 2)
 }
