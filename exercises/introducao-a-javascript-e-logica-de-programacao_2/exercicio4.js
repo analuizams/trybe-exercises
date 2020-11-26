db9fd3c7050f9d86dbf13167cdc0d42646d188f1
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2020-11-26 17:58:59 -0300

    Adiciona exercicios 4, 5 e 6

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio4.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (index = 0; index < numbers.length; index += 1) {
+    result += numbers[index];
+}
+
+let average = result / numbers.length;
+
+if (average > 20) {
+    console.log('valor maior que 20')
+} else {
+    console.log('valor menor ou igual a 20')
+}
