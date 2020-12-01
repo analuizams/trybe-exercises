cea5000957dd1880498a31545cbe351bface65aa
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2020-12-01 02:14:24 -0300

    Refatora exercicio 1 - parte 1 JS

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio1.js
@@ -1,4 +1,4 @@
-let a =10;
+/* let a =10;
 let b = 2;
 
 console.log('Soma: ' + (a + b));
@@ -6,3 +6,36 @@ console.log('Subtração: ' + (a - b));
 console.log('Multiplicação: ' + (a * b));
 console.log('Divisão: ' + (a / b));
 console.log('Módulo: ' + (a % b));
+
+REFATORADO:
+*/
+
+function sum(num1, num2) {
+    return (num1 + num2);
+}
+
+console.log('Soma: ' + sum(10, 2));
+
+function subtraction(num1, num2) {
+    return (num1 - num2);
+}
+
+console.log('Subtração: ' + subtraction(10, 2));
+
+function multiplication(num1, num2) {
+    return (num1 * num2);
+}
+
+console.log('Multiplicação: ' + multiplication(10, 2));
+
+function division(num1, num2) {
+    return (num1 / num2);
+}
+
+console.log('Divisão: ' + division(10, 2));
+
+function modulus(num1, num2) {
+    return (num1 % num2);
+}
+
+console.log('Módulo: ' + modulus(10, 2));
