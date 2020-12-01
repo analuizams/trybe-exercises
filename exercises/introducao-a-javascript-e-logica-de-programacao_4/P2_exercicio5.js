1f908e2a5cdccb0f226bbe5368543e2e0db2c030
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2020-12-01 03:48:47 -0300

    Adiciona exercicios 5 e 6 - parte 2

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/P2_exercicio5.js
@@ -0,0 +1,17 @@
+/*
+Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+Array de teste: [2, 3, 2, 5, 8, 2, 3]; .
+Valor esperado no retorno da função: 2 .
+*/
+
+function mostRepeatedNum(numbers) {
+    let mostRepeatedNum = numbers[0]
+    for (let index in names) {
+        if (longerName.length < names[index].length) {
+            longerName = names[index];
+        }
+    }
+    return longerName
+}
+
+console.log(longerName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/P2_exercicio6.js
@@ -0,0 +1,15 @@
+/*
+Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
+Valor de teste: N = 5 .
+Valor esperado no retorno da função: 1+2+3+4+5 = 15 .
+*/
+
+function sumAllNumbers(number) {
+    let sum = 0;
+    for (let index = 0; index <= number; index += 1) {
+    sum = sum + index;    
+    }
+    return sum;
+}
+
+console.log(sumAllNumbers(5));
